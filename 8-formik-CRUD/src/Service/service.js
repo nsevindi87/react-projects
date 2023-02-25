6fefa313308c860f22b230cecfc2208e4dd2aac4
Nizami Sevindi <nsevindi87@gmail.com>
2023-02-25 23:00:26 +0100

    projects

--- 8-formik-CRUD/src/Service/service.js
+++ 8-formik-CRUD/src/Service/service.js
@@ -3,52 +3,55 @@
   const BASE_URL = 'http://dev.hicoders.cloud/playground/api/v1/User'
 
 
-//======= GET Participant =================================
+//!======= GET Participant =================================
 export const getParticipants = async () => {
-    try {
-        const response = await fetch(BASE_URL);
-        const json = await response.json()
-        if(response.ok){
-          return json
-        }else{
-          throw new Error(response.message)
-        }
-    } catch (error) {
-      throw error
+  try {
+    const response = await fetch(BASE_URL);
+    const json = await response.json()
+    if(response.ok){
+      return json
+    }else{
+      throw new Error(response.message)
     }
+  } catch (error) {
+    throw error
   }
+}
 
+//!======= CREATE Participant =================================
 export const createParticipant = async (pParticipant) => {
-    const response = await fetch(BASE_URL, {
-        method: 'POST',
-        body: JSON.stringify(pParticipant),
-        headers: { 'Content-Type': 'application/json'},
+  const response = await fetch(BASE_URL, {
+    method: 'POST',
+    body: JSON.stringify(pParticipant),
+    headers: { 'Content-Type': 'application/json'},
+  });
+  const json = await response.json();
+  if(response.ok){
+    return json
+  }else{
+    throw new Error(response.message)
+  }
+}
+
+//!======= DELETE Participant =================================
+export const deleteParticipant = async (id) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE',
     });
     const json = await response.json();
+    console.log("tiklandi")
     if(response.ok){
-        return json
-      }else{
-        throw new Error(response.message)
-      }
-}
-
-export const deleteParticipant = async (id) => {
-    try {
-        const response = await fetch(`${BASE_URL}/${id}`, {
-            method: 'DELETE',
-        });
-        const json = await response.json();
-        console.log("tiklandi")
-        if(response.ok){
-            return json
-          }else{
-            throw new Error(response.message)
-          }
-    }catch(error){
-        throw error
+      return json
+    }else{
+      throw new Error(response.message)
     }
+  }catch(error){
+    throw error
+  }
 }
 
+//!======= UPDATE Participant =================================
 export const updateParticipant = async (pId, pUpdatedParticipant) => {
   try {
     const response = await fetch(`${BASE_URL}/${pId}`,{
